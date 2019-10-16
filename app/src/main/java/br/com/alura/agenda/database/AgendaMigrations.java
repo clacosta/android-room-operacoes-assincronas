d54dbbd6b4693d858030f3aa18d7ebf1a68b0ab9
Claudio Costa <claudio@imaginaction.com.br>
2019-10-16 14:18:55 -0300

    feet: 02 - 05 - Aplicando migration das novas entidades

--- app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
@@ -4,6 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import br.com.alura.agenda.model.TipoTelefone;
+
+import static br.com.alura.agenda.model.TipoTelefone.FIXO;
+
 class AgendaMigrations {
 
     private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
@@ -21,6 +25,7 @@ class AgendaMigrations {
                     "`nome` TEXT, " +
                     "`telefone` TEXT, " +
                     "`email` TEXT)");
+
             // Copiar dados da tabela antiga para a nova
             database.execSQL("INSERT INTO `Aluno_Novo` (" +
                     "`id`, " +
@@ -28,8 +33,10 @@ class AgendaMigrations {
                     "`telefone`, " +
                     "`email`) " +
                     "SELECT `id`, `nome`, `telefone`, `email` FROM `Aluno`");
+
             // Remove tabela antiga
             database.execSQL("DROP TABLE `Aluno`");
+
             // Renomear a tabela nova com o nome da tabela antiga
             database.execSQL("ALTER TABLE `Aluno_Novo` RENAME TO `Aluno`");
         }
@@ -40,7 +47,7 @@ class AgendaMigrations {
             database.execSQL("ALTER TABLE `Aluno` ADD COLUMN `momentoDeCadastro` INTEGER");
         }
     };
-    private static final Migration MIGRATION_4_5 = new Migration(4 , 5) {
+    private static final Migration MIGRATION_4_5 = new Migration(4, 5) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
             database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_Novo` (" +
@@ -50,14 +57,66 @@ class AgendaMigrations {
                     "`telefoneCelular` TEXT, " +
                     "`email` TEXT, " +
                     "`momentoDeCadastro` INTEGER)");
+
             database.execSQL("INSERT INTO `Aluno_Novo` (" +
                     "`id`, " +
                     "`nome`, " +
                     "`telefoneFixo`, " +
                     "`email`, " +
                     "`momentoDeCadastro`) " +
-                    "SELECT `id`, `nome`, `telefone`, `email`, `momentoDeCadastro` FROM `Aluno`");
+                    "SELECT " +
+                    "`id`, " +
+                    "`nome`, " +
+                    "`telefone`, " +
+                    "`email`, " +
+                    "`momentoDeCadastro` " +
+                    "FROM `Aluno`");
+
             database.execSQL("DROP TABLE `Aluno`");
+
+            database.execSQL("ALTER TABLE `Aluno_Novo` RENAME TO `Aluno`");
+        }
+    };
+    private static final Migration MIGRATION_5_6 = new Migration(5, 6) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_Novo` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`nome` TEXT, " +
+                    "`email` TEXT, " +
+                    "`momentoDeCadastro` INTEGER)");
+
+            database.execSQL("INSERT INTO `Aluno_Novo` (" +
+                    "`id`, " +
+                    "`nome`, " +
+                    "`email`, " +
+                    "`momentoDeCadastro`) " +
+                    "SELECT " +
+                    "`id`, " +
+                    "`nome`, " +
+                    "`email`, " +
+                    "`momentoDeCadastro` " +
+                    "FROM `Aluno`");
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Telefone` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`numero` TEXT, " +
+                    "`tipo` TEXT, " +
+                    "`alunoId` INTEGER NOT NULL)");
+
+            database.execSQL("INSERT INTO `Telefone` (" +
+                    "`numero`, " +
+                    "`alunoId`) " +
+                    "SELECT " +
+                    "`telefoneFixo`, " +
+                    "`id` " +
+                    "FROM `Aluno`");
+
+            database.execSQL("UPDATE `Telefone` SET " +
+                    "`tipo` = ? ", new TipoTelefone[]{FIXO});
+
+            database.execSQL("DROP TABLE `Aluno`");
+            
             database.execSQL("ALTER TABLE `Aluno_Novo` RENAME TO `Aluno`");
         }
     };
@@ -66,7 +125,8 @@ class AgendaMigrations {
             MIGRATION_1_2,
             MIGRATION_2_3,
             MIGRATION_3_4,
-            MIGRATION_4_5
+            MIGRATION_4_5,
+            MIGRATION_5_6
     };
 
 }
